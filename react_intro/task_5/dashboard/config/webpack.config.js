54e62abd82ec81082e1b4938908628f2589e1e62
Esther447 <esther@example.com>
2026-09-11 23:03:03 +0200

    react_intro: add tasks 6, 7, 8 - React tests, GitHub Pages deploy, Webpack project

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, '../src/index.js'),
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, '../dist'),
+    hot: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          { loader: 'file-loader' },
+          { loader: 'image-webpack-loader', options: { disable: true } },
+        ],
+      },
+    ],
+  },
+};
---
+++ react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,4 @@
+import './style.css';
+
+const root = document.getElementById('root');
+root.innerHTML = '<h1>Dashboard</h1>';
